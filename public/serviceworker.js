fc07f992e191170a32e83dbe59654de5508983d6
Wuzup <dgr993@gmail.com>
2020-10-01 21:50:30 -0600

    added db.js and serviceworker to html

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,60 +1,91 @@
-const CACHE = "my-site-cache-v1";
-const DATA_NAME = "data-cache-v1";
+
 const FILES_TO_CACHE = [
     "./index.html",
     "./styles.css",
     "./icons/icon-192x192.png",
     "./icons/icon-512x512.png",
-    "./index.js"
+    "./index.js",
+    "./db.js"
 ];
 
-self.addEventListener("install",(event) => {
+  
+  const STATIC_CACHE = "static-cache-v1";
+  const RUNTIME_CACHE = "runtime-cache";
+  
+  self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(CACHE)
-        .then((cache)=> {
-            console.log(CACHE);
-            return cache.addAll(FILES_TO_CACHE)
+      caches
+        .open(STATIC_CACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+  });
+  
+  // The activate handler takes care of cleaning up old caches.
+  self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+      caches
+        .keys()
+        .then(cacheNames => {
+          // return array of cache names that are old to delete
+          return cacheNames.filter(
+            cacheName => !currentCaches.includes(cacheName)
+          );
         })
-    )
-})
-
-self.addEventListener("fetch",(event) => {
-    if (event.request.url.includes("/api")){
-        event.respondWith(caches.open(
-            CACHE
-        )
-        .then((cache)=> {
-            return fetch(event.request(cache))
-            .then((response)=> {
-                if(response.status===200) {
-                    cache.put(event.request.url,response.clone())
-                }
-                return response
-            }).catch((err)=> {
-                return cache.match(event.request(err))
+        .then(cachesToDelete => {
+          return Promise.all(
+            cachesToDelete.map(cacheToDelete => {
+              return caches.delete(cacheToDelete);
             })
-            .catch((err)=>
-            console.log(err));
-        }))
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
+  
+  self.addEventListener("fetch", event => {
+    // non GET requests are not cached and requests to other origins are not cached
+    if (
+      event.request.method !== "GET" ||
+      !event.request.url.startsWith(self.location.origin)
+    ) {
+      event.respondWith(fetch(event.request));
+      return;
     }
-})
-
-self.addEventListener("activate",(event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+  
+    // handle runtime GET requests for data from /api routes
+    if (event.request.url.includes("/api/images")) {
+      // make network request and fallback to cache if network request fails (offline)
+      event.respondWith(
+        caches.open(RUNTIME_CACHE).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              cache.put(event.request, response.clone());
+              return response;
+            })
+            .catch(() => caches.match(event.request));
+        })
+      );
+      return;
+    }
+  
+    // use cache first for all other requests for performance
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+  
+        // request is not in cache. make network request and cache the response
+        return caches.open(RUNTIME_CACHE).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
       })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
-  );
-})
-
+    );
+  });
+  
